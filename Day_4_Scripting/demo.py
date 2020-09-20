865ec4fcd901e442e3e7601d25802fb806dd5bdc
Shaurya sinha <60511797+ShauryaSinhaML@users.noreply.github.com>
2020-09-20 19:39:41 +0530

    Create demo.py

---
+++ Day_4_Scripting/demo.py
@@ -0,0 +1,16 @@
+import useful_functions as uf
+
+print(uf.num)
+
+scores = [88, 92, 79, 93, 85]
+
+mean = uf.mean(scores)
+curved = uf.add_five(scores)
+
+mean_c = uf.mean(curved)
+
+print("Scores:", scores)
+print("Original Mean:", mean, " New Mean:", mean_c)
+
+# print(__name__)
+# print(uf.__name__)
