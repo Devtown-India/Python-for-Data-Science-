3c848ea97a81e50501658ce3b7b93098c5faf478
Shaurya sinha <60511797+ShauryaSinhaML@users.noreply.github.com>
2020-09-20 18:36:58 +0530

    Create error_exception1.py

---
+++ Day_4_Scripting/error_exception1.py
@@ -0,0 +1,10 @@
+"""
+Exceptions occur when unexpected things happen during execution of a program, 
+even if the code is syntactically correct. 
+There are different types of built-in exceptions in Python, 
+and you can see which exception is thrown in the error message.
+"""
+
+x = int(input("Enter a number: "))
+x += 20
+print(x)
