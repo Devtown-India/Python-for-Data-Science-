e83d229b9d7a59e2ad4f1a17a71bd7311b7b8cd0
Shaurya sinha <60511797+ShauryaSinhaML@users.noreply.github.com>
2020-09-20 18:36:23 +0530

    Create error_syntax.py

---
+++ Day_4_Scripting/error_syntax.py
@@ -0,0 +1,9 @@
+"""
+Syntax errors occur when Python can’t interpret our code, 
+since we didn’t follow the correct syntax for Python. 
+These are errors you’re likely to get when you make a typo, 
+or you’re first starting to learn Python.
+"""
+
+msg = "Welcome to the ShapeAI developer community
+print msg
