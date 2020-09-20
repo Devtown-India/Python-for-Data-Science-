921ab111a69a946cf9c38184465ed25f278a3a33
Shaurya sinha <60511797+ShauryaSinhaML@users.noreply.github.com>
2020-09-20 17:35:01 +0530

    Create snakes.py

---
+++ Day_4_Scripting/snakes.py
@@ -0,0 +1,16 @@
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, shaurya
+"""
+
+
+print(snake_string * how_many_snakes)
