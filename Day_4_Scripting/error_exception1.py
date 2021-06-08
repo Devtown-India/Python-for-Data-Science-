440d25312a9f24f1cd13631ae9e1d0b24825d4a4
Shaurya sinha <60511797+ShauryaSinhaML@users.noreply.github.com>
2021-06-08 15:07:33 +0530

    5th

--- Day_4_Scripting/error_exception1.py
+++ Day_4_Scripting/error_exception1.py
@@ -1,7 +1,7 @@
 """
 Exceptions occur when unexpected things happen during execution of a program, 
 even if the code is syntactically correct. 
-There are different types of built-in exceptions in Python, 
+There are different types of built-in exceptions in Python, .
 and you can see which exception is thrown in the error message.
 """
 
